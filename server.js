c61896d610e841644b819b59b48686a5155afb07
breakfastitem <andrew.ehrman@gmail.com>
2021-03-16 20:19:06 -0400

    Set up start up script

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const express= require("express");
+const express = require("express");
+const path =require("path");
 
-const app =express();
+const app = express();
 
 
-const PORT= process.env.EXPRESS_PORT ||8000;
+const PORT = process.env.EXPRESS_PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Link express server to react build
+app.use(express.static("./client/build/static"));
+
 app.listen(PORT);
 
-console.log(`server is listening on port ${PORT}`);
+console.log(`server is listening http://localhost:${PORT}`);
