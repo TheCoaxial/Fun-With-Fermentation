f91625fed98735deb4647b5e7fdf8d1db2132092
breakfastitem <andrew.ehrman@gmail.com>
2021-03-18 19:20:34 -0400

    Sql server is initalized on npm start in development

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const path =require("path");
 
 const app = express();
 
+const db = require("./models");
+
 
 const PORT = process.env.EXPRESS_PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Link express server to react build
-app.use(express.static("./client/build/static"));
 
-app.listen(PORT);
 
-console.log(`server is listening http://localhost:${PORT}`);
+db.sequelize.sync().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`server is listening http://localhost:${PORT}`);
+    });
+});
