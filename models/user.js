d1d58bc74408fd73b3011d48ee291f7419dd4129
breakfastitem <andrew.ehrman@gmail.com>
2021-03-18 19:03:03 -0400

    Added non relational aspects of user object

---
+++ models/user.js
@@ -0,0 +1,38 @@
+module.exports = function (sequelize, DataTypes) {
+    const User = sequelize.define("User", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        bio: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: ""
+        },
+        contributionScore: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        }
+    });
+
+};
