e31e5b23a970154f48e51425f2eab58c753e9c47
pjdip <pjdip1@gmail.com>
2021-03-18 21:11:51 -0400

    adding Footer and Container components, adding brew,profile,feed pages, adding react router to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Container from "../src/components/Container";
+import Brew from "../src/pages/Brew";
+import Profile from "../src/pages/Profile";
+import Feed from "../src/pages/Feed";
+import Footer from "../src/components/Footer";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Container>
+                    <Route exact path="/brew" component={Brew} />
+                    <Route exact path="/profile" component={Profile} />
+                    <Route exact path="/feed" component={Feed} />
+                </Container>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
