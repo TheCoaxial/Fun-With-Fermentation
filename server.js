b497b33e6fc5257b68eb345df12f027a3ef6f9af
Coaxial <thecoaxial@gmail.com>
2021-03-18 20:27:33 -0400

    adding auth

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const passport = require("passport");
 const express = require("express");
 const path =require("path");
 
@@ -6,8 +8,12 @@ const app = express();
 
 const PORT = process.env.EXPRESS_PORT || 3001;
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(passport.initialize());
+// Passport config
+passport.use( require("./config/jwtPassportStrategy") );
 
 //Link express server to react build
 app.use(express.static("./client/build/static"));
