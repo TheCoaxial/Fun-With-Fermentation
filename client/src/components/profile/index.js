b7f9747459409a0ce54593b7113e92b2cda5a8cc
Coaxial <thecoaxial@gmail.com>
2021-03-20 11:02:47 -0400

    Refactoring

--- client/src/components/profile/index.js
+++ client/src/components/profile/index.js
@@ -5,39 +5,34 @@ export default class Profile extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      currentUser: AuthService.getCurrentUser()
-    };
-  }
+        this.state = {
+        currentUser: AuthService.getCurrentUser()
+        };
+    }
 
   render() {
     const { currentUser } = this.state;
 
+    // Currently just displays Info about the user from the DB
     return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3>
-            <strong>{currentUser.username}</strong> Profile
-          </h3>
-        </header>
-        <p>
-          <strong>Token:</strong>{" "}
-          {currentUser.accessToken.substring(0, 20)} ...{" "}
-          {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-        </p>
-        <p>
-          <strong>Id:</strong>{" "}
-          {currentUser.id}
-        </p>
-        <p>
-          <strong>Email:</strong>{" "}
-          {currentUser.email}
-        </p>
-        <strong>Authorities:</strong>
-        <ul>
-          {currentUser.roles &&
-            currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-        </ul>
+      <div className="">
+            <header className="">
+                <h3>
+                    <strong>{currentUser.username}</strong> Profile
+                </h3>
+            </header>
+            <p>
+                <strong>Web Token:</strong>{" "}
+                {currentUser.accessToken}
+            </p>
+            <p>
+                <strong>User Id in DB:</strong>{" "}
+                {currentUser.id}
+            </p>
+            <p>
+                <strong>User Email:</strong>{" "}
+                {currentUser.email}
+            </p>
       </div>
     );
   }
