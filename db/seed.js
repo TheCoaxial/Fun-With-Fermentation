ea5e5af68816b19f732b3c148ab39b27a90351f1
breakfastitem <andrew.ehrman@gmail.com>
2021-03-20 14:19:22 -0400

    Added authenticaton to seeds

--- db/seed.js
+++ db/seed.js
@@ -7,10 +7,12 @@ async function insertData() {
     /**
      * Users
      */
-    await insertUser("Username1", "username1@email.com", "hashedpassword");
-    await insertUser("Username2", "username2@email.com", "hashedpassword");
-    await insertUser("Username3", "username3@email.com", "hashedpassword");
+    let user1Token = await insertUser("Username1", "username1@email.com", "hashedpassword");
+    let user2Token = await insertUser("Username2", "username2@email.com", "hashedpassword");
+    let user3Token = await insertUser("Username3", "username3@email.com", "hashedpassword");
 
+
+    console.log(user1Token);
     /**
      * Brews
      */
@@ -18,16 +20,52 @@ async function insertData() {
     await insertBrew("Username1's second Brew", "Username1", 1);
     await insertBrew("Username2's first Brew", "Username2", 2);
 
+
+    /**
+   * Comments
+   */
+    // await insertComment("WOW INCREDIBLE", "Username1", 1, 3);
+    // await insertComment("This Is my brew", "Username1", 1, 2);
+    // await insertComment("This Is Username3's comment", "Username3", 3, 1);
+
+
     return;
 }
 
 async function insertUser(userName, email, password) {
-    await fetch("http://localhost:3001/api/new-user", {
+    await fetch("http://localhost:3001/api/auth/signup", {
         method: "POST",
         body: JSON.stringify({
-            name: userName,
+            username: userName,
             password: password,
             email: email
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+
+    let res = await fetch("http://localhost:3001/api/auth/signin", {
+        method: "POST",
+        body: JSON.stringify({
+            username: userName,
+            password: password
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    return res;
+}
+
+async function insertBrew(brewName, author, UserId) {
+    await fetch(`http://localhost:3001/api/${UserId}/new-brew`, {
+        method: "POST",
+        body: JSON.stringify({
+            name: brewName,
+            author: author
         }),
         headers: {
             'Content-Type': 'application/json',
@@ -37,12 +75,12 @@ async function insertUser(userName, email, password) {
     return;
 }
 
-async function insertBrew(brewName, author, UserId) {
-    await fetch(`http://localhost:3001/api/${UserId}/new-brew`, {
+async function insertComment(body, author, UserId, BrewId) {
+    await fetch(`http://localhost:3001/api/${UserId}/${BrewId}/new-comment`, {
         method: "POST",
         body: JSON.stringify({
-            name: brewName,
-            author: author
+            author: author,
+            body: body
         }),
         headers: {
             'Content-Type': 'application/json',
