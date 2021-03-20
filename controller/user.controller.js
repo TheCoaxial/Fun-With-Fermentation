bfcbaef43e9b24de6d48b13aaf298b57217b732b
Coaxial <thecoaxial@gmail.com>
2021-03-20 10:22:04 -0400

    adding auth controller/user routes

---
+++ controller/user.controller.js
@@ -0,0 +1,8 @@
+exports.allAccess = (req, res) => {
+    res.status(200).send("Public Content.");
+  };
+  
+  exports.userBoard = (req, res) => {
+    res.status(200).send("User Content.");
+  };
+  
