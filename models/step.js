e468c41666de650aba4d5eaa0e882efe79fb8020
pjdip <pjdip1@gmail.com>
2021-03-25 17:46:53 -0400

    step model

---
+++ models/step.js
@@ -0,0 +1,25 @@
+module.exports = function (sequelize, DataTypes) {
+    const Step = sequelize.define("Step", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        duration: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        instructions: {
+            type: DataTypes.TEXT,
+            length: "long",
+            allowNull: false
+        }
+    });
+
+    Step.associate = models => {
+        Step.belongsTo(models.Brew);
+    };
+
+    return Step;
+};
