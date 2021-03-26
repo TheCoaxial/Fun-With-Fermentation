3577d9a5cc63bae08c5a6de30db0f2aace7f9cc7
pjdip <pjdip1@gmail.com>
2021-03-26 12:30:04 -0400

    utils api matching get methods to get routes in api-routes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -5,9 +5,7 @@ class API {
     axios;
 
     constructor() {
-
         this.axios = axios.create();
-
     }
 
     /**
@@ -17,13 +15,9 @@ class API {
     setHeader( name, value ) {
 
         if( value )
-
             this.axios.defaults.headers.common[name] = value;
-
         else
-
             delete this.axios.defaults.headers.common[name];
-
     }
 
     /**
@@ -34,7 +28,6 @@ class API {
      * @returns {Promise}
      */
     register( userData ) {
-
         return this.axios.post("/api/register", userData);
     }
 
@@ -47,63 +40,67 @@ class API {
      * @returns {Promise}
      */
     login( userData ) {
-
         return this.axios.post("/api/login", userData);
     }
 
     authenticated() {
-
         return this.axios.post("/api/authenticated");
     }
 
-    postBrew(userID) {
-
-        return this.axios.post("/api/" + userID + "/new-brew");
-    }
-
-    saveNewFavorite(brewID, userID) {
-
-        return this.axios.post("/api/favorite/" + brewID + "/" + userID );
-    }
+    // GET
 
     getUserProfile(userId) {
-        
         return this.axios.get("/api/user/" + userId);
     }
 
-    getUserBrews(userId) {
+    getAllBrews() {
+        return this.axios.get("/api/brew/all");
+    }
 
+    getUserBrews(userId) {
         return this.axios.get("/api/brew/" + userId);
     }
 
     getSpecificBrew(brewID) {
-
         return this.axios.get("/api/brew/" + brewID);
     }
 
-    deleteBrew(brewID) {
+    getUserFavorites(userID) {
+        return this.axios.get("/api/favorite/" + userID);
+    }
 
+    getSpecificFavorite(userID, brewID) {
+        return this.axios.get("/api/favorite/" + brewID + "/" + userID);
+    }
+
+    // POST
+
+    postBrew(userID) {
+        return this.axios.post("/api/" + userID + "/new-brew");
+    }
+
+    saveNewFavorite(brewID, userID) {
+        return this.axios.post("/api/favorite/" + brewID + "/" + userID );
+    }
+
+    // DELETE
+
+    deleteBrew(brewID) {
         return this.axios.delete("/api/delete-brew/" + brewID);
     }
 
     deleteComment(commentID) {
-
         return this.axios.delete("/api/delete-comment/" + commentID);
     }
 
     deleteFavorite(brewID, userID) {
-
         return this.axios.delete("/api/delete-favorite/" + brewID + "/" + userID);
     }
 
+    // UPDATE
+
     updateComment(commentID) {
-        
         return this.axios.put("/api/update-comment/" + commentID);
-    }
-
-    getUserFavorites(userID) {
-
-        return this.axios.get("/api/favorite/" + userID);
     }
 
 }
