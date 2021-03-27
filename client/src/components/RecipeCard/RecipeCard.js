1c4f10cb3b81b8b27d196ce125a1f6189d15e1fd
lbmann <l.bmann@yahoo.com>
2021-03-27 15:27:07 -0400

    new material ui recipe cards and feed styling

--- client/src/components/RecipeCard/RecipeCard.js
+++ client/src/components/RecipeCard/RecipeCard.js
@@ -1,8 +1,57 @@
 import React from "react";
 import "./RecipeCard";
 import "../../App.css";
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }));
 
 export default function RecipeCard({ id, UserId, name, description, author }) {
+
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+      };
+
+    let userLink = <span>Created by <a href={`/user/${UserId}`}>{author}</a></span>;
+      
 
     // const ingredientMap = () => {
     //     if (ingredients.length) {
@@ -21,17 +70,56 @@ export default function RecipeCard({ id, UserId, name, description, author }) {
     // };
 
     return (
-        <div className="recipeCard">
-            <h3 className="title">
-                <a href={`/brews/${id}`}>{name}</a>
-            </h3>
-            <h6>Created by <a href={`/user/${UserId}`}>{author}</a></h6>
-            <ul>
-                {/* {ingredientMap()} */}
-            </ul>
-            <p className="description truncate-overflow">
+        // <div className="recipeCard">
+        //     <h3 className="title">
+        //         <a href={`/brews/${id}`}>{name}</a>
+        //     </h3>
+        //     <h6>Created by <a href={`/user/${UserId}`}>{author}</a></h6>
+        //     <ul>
+        //         {/* {ingredientMap()} */}
+        //     </ul>
+        //     <p className="description truncate-overflow">
+        //         {description}
+        //     </p>
+        // </div>
+        <div>
+            <Card className={`recipeCard ${classes.root}`}>
+            <CardHeader
+            title={name}
+            subheader={userLink}
+            />
+            <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
                 {description}
-            </p>
+            </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+                <ShareIcon />
+            </IconButton>
+            <IconButton
+                className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+            >
+                <ExpandMoreIcon />
+            </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+                <Typography paragraph>Instructions</Typography>
+                <Typography paragraph>
+                    Show recipe ingredients and instructions here if warranted.
+                </Typography>
+            </CardContent>
+            </Collapse>
+            </Card>
         </div>
     );
 };
--- client/src/pages/Feed/Feed.js
+++ client/src/pages/Feed/Feed.js
@@ -88,10 +88,19 @@ export default function Feed() {
 
             <Grid container spacing={3}>
                 <Grid item xs={4} className="sidebarWrap">
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Top Users
-                    </Typography>
-                    {topUsersJSX}
+                    <div className="innerSidebarWrap">
+                        <Typography gutterBottom variant="h5" component="h1">
+                            Top Users
+                        </Typography>
+                        {topUsersJSX}
+                    </div>
+
+                    <div className="innerSidebarWrap">
+                        <Typography gutterBottom variant="h5" component="h1">
+                            Top Recipes
+                        </Typography>
+                        {feedBrewsJSX}
+                    </div>
                 </Grid>
                 <Grid item xs={8}>
                     {feedBrewsJSX}
