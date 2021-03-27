169fe15e2c42e28940a8d606ea0a58fb125ee3dc
breakfastitem <andrew.ehrman@gmail.com>
2021-03-26 20:46:50 -0400

    Fixed favorites

--- models/favorite.js
+++ models/favorite.js
@@ -2,9 +2,8 @@ module.exports = function (sequelize) {
     const Favorite = sequelize.define("Favorite", {});
 
     Favorite.associate = (models) => {
-
-        Favorite.hasMany(models.Brew);
-        Favorite.hasMany(models.User);
+        Favorite.belongsTo(models.Brew);
+        Favorite.belongsTo(models.User);
 
     };
 
