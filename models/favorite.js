6831099f25a01d667c62df2c628e83bc47e5b19f
Coaxial <thecoaxial@gmail.com>
2021-03-26 20:33:09 -0400

    passing it along

--- models/favorite.js
+++ models/favorite.js
@@ -2,10 +2,8 @@ module.exports = function (sequelize) {
     const Favorite = sequelize.define("Favorite", {});
 
     Favorite.associate = (models) => {
-
-        Favorite.hasMany(models.Brew);
-        Favorite.hasMany(models.User);
-
+        // Favorite.hasMany(models.Brew);
+        // Favorite.hasMany(models.User);
     };
 
     return Favorite;
