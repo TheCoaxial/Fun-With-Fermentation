b85e53ca5260be86b177ebd97bec41432af2bcca
pjdip <pjdip1@gmail.com>
2021-04-03 00:48:52 -0400

    cascade delete everywhere?

--- models/commentReply.js
+++ models/commentReply.js
@@ -2,7 +2,7 @@ module.exports = function (sequelize) {
     const CommentReply = sequelize.define("CommentReply", {});
 
     CommentReply.associate = (models) => {
-        CommentReply.belongsTo(models.User, { foreignKey: 'parent', as: 'Parent' });
+        CommentReply.belongsTo(models.User, { foreignKey: 'parent', as: 'Parent', onDelete: 'cascade' });
         CommentReply.hasMany(models.User, { foreignKey: 'child', as: 'Child', onDelete: 'cascade' });
     };
 
