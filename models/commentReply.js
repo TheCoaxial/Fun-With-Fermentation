2483c60bd6c88f4c09db60d6e969cb852fbee140
pjdip <pjdip1@gmail.com>
2021-04-03 00:46:16 -0400

    ondeleteCascade

--- models/commentReply.js
+++ models/commentReply.js
@@ -3,7 +3,7 @@ module.exports = function (sequelize) {
 
     CommentReply.associate = (models) => {
         CommentReply.belongsTo(models.User, { foreignKey: 'parent', as: 'Parent' });
-        CommentReply.hasMany(models.User, { foreignKey: 'child', as: 'Child' });
+        CommentReply.hasMany(models.User, { foreignKey: 'child', as: 'Child', onDelete: 'cascade' });
     };
 
     return CommentReply;
