7660769d8d5f692535683de9b4070b4ca3e05bd2
Coaxial <thecoaxial@gmail.com>
2021-04-04 18:47:53 -0400

    refresh fix atempt 1

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.get("/", (req, res) => {
 
 let PORT = process.env.EXPRESS_PORT || 3001;
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 if (process.env.NODE_ENV === "production") {
   PORT = process.env.PORT;
 }
