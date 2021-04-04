394675b573c68595cfeedbdc74710dc258f69fa1
Coaxial <thecoaxial@gmail.com>
2021-04-04 18:53:39 -0400

    refresh fix atempt 2

--- server.js
+++ server.js
@@ -31,13 +31,13 @@ app.use(express.json());
 
 app.use(express.static('./client/build'));
 // Simple test route
-app.get("/", (req, res) => {
-  res.json({ message: "Test route" });
-});
+// app.get("/", (req, res) => {
+//   res.json({ message: "Test route" });
+// });
 
 let PORT = process.env.EXPRESS_PORT || 3001;
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
