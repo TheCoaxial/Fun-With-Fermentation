866f1a974c31339c2c5d298b0c04660364ffe7ae
Coaxial <thecoaxial@gmail.com>
2021-04-04 19:00:29 -0400

    atempt 3

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.static('./client/build'));
 
 let PORT = process.env.EXPRESS_PORT || 3001;
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
